6f8a8bd835fa257a22f76b91eb0641fd6df587e0
rpiflv <rpiflv@gmail.com>
2022-11-08 21:56:40 +0900

    add static folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 require('dotenv').config({ path: './.env' })
 
+const cors = require('cors')
+
+
+app.use(express.json())
+app.use(cors())
+app.use("/", express.static(__dirname + "/public"));
+
+
+
+app.get('/api/teams', async (req, res) => {
+    await fetch('https://data.nba.net/data/10s/prod/v1/2022/teams.json')
+        .then((fetchedData) => fetchedData.json())
+        .then(data => res.send(data.league.standard))
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port: ${process.env.PORT}`)
