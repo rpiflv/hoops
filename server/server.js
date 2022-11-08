657d19c8fc0599694eb029f110eee7bd5b84838f
rpiflv <rpiflv@gmail.com>
2022-11-08 22:30:12 +0900

    serv build folder

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
-app.use("/", express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/build"));
 
 
 
