2d01f90dc74d1fe41fe9424db5ab932d82f6ed57
rpiflv <rpiflv@gmail.com>
2022-11-09 08:52:21 +0900

    change path on server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
-app.use("/", express.static(__dirname + "../client/build")); // "/public"
+// app.use("/", express.static(__dirname + "../client/build")); // "/public"
+
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
