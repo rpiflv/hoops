305c2d0c05955eac6b236f31a0733b2b1b2c9334
rpiflv <rpiflv@gmail.com>
2022-11-09 19:19:54 +0900

    set knex and initial migration file

---
+++ server/db/knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PWD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./migrations",
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ server/knex.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const config = require('./db/knexfile')
+const knex = require('knex')
+
+const environment = process.env.NODE_ENV;
+module.exports = knex(config[environment]);
