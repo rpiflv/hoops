ee9abf62f2b8d3c22a7ae246c893c157c416405f
rpiflv <rpiflv@gmail.com>
2022-11-10 12:53:31 +0900

    add favorite component

--- client/src/components/Favorite.js
+++ client/src/components/Favorite.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Link } from "react-router-dom";
 
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || '';
