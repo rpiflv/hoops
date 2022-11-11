65bd66df1db215663d17363bb92fcd6e7eaca3f0
rpiflv <rpiflv@gmail.com>
2022-11-12 07:26:51 +0900

    change knexfile

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -4,25 +4,29 @@ require('dotenv').config({ path: '../../.env' })
  */
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PWD
-    },
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: process.env.DATABASE_NAME,
+  //     user: process.env.DATABASE_USER,
+  //     password: process.env.DATABASE_PWD
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./migrations",
+  //   }
+  // },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
-    migrations: {
-      directory: "./migrations",
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./migrations",
     },
