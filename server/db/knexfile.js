fbe828a57f091aed55a81398adaa8644ce6acd6e
rpiflv <rpiflv@gmail.com>
2022-11-11 19:31:36 +0900

    knexfile update

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -23,13 +23,12 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: "./migrations",
-    }
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   }
 
 };
