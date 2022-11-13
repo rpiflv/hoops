bfe28346a944c8892c7279dd936be85fe426ada3
rpiflv <rpiflv@gmail.com>
2022-11-13 22:30:32 +0900

    fix navbar link

--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
-
-
 
 function NavMenu() {
     return (
         <>
 
-            <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
+            <Navbar expand="lg" bg="light" variant="light" sticky="top">
                 <Container>
                     <Navbar.Brand >
                         <Link to="/">
@@ -17,10 +17,14 @@ function NavMenu() {
                         </Link>
                     </Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link to="/teams">Teams</Nav.Link>
+                        <LinkContainer to="/teams">
+                            <Nav.Link >Teams</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                     <Nav className="">
-                        <Nav.Link to="/myplayers">My players</Nav.Link>
+                        <LinkContainer to="/myplayers">
+                            <Nav.Link>My players</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
