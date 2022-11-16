331fa84b77337abcf75ec47df535b05e000481f1
rpiflv <rpiflv@gmail.com>
2022-11-16 11:58:28 +0900

    add user migration

---
+++ server/db/migrations/20221114093249_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('users', function (table) {
+            table.increments("id");
+            table.string("username", 24);
+            table.string("email", 56);
+            table.string("password", 256)
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('users')
+};
