447d1c55c38afa9d7f408c8321dbf1ef38d72cea
rpiflv <67497636+rpiflv@users.noreply.github.com>
2023-03-06 14:37:51 +0900

    logo and navbar update

--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -22,22 +22,24 @@ function NavMenu(props) {
     }
     return (
         <>
-            <Navbar expand="lg" bg="light" variant="light" sticky="top" className="navbarGrad">
+            <Navbar expand="lg" sticky="top" className="navBarBlack">
                 <Container>
                     <Navbar.Brand >
                         <Link to="/">
-                            <img src="./hoopsPNG.png" className="d-inline-block align-top" />
+                        <img src="./hoopsBlack.png" className="d-inline-block align-top" height={100}/>
+
+                            {/* <img src="./hoopsLogoTransp.png" className="d-inline-block align-top" /> */}
                         </Link>
                     </Navbar.Brand>
                     <Nav className="me-auto">
                         <LinkContainer to="/teams">
-                            <Nav.Link >NBA Teams</Nav.Link>
+                            <Nav.Link bsPrefix="navLink">NBA Teams</Nav.Link>
                         </LinkContainer>
                     </Nav>
                     {user &&
                         <Nav>
                             <LinkContainer to="/myplayers">
-                                <Nav.Link>My players</Nav.Link>
+                                <Nav.Link bsPrefix="navLink">My players</Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/">
                                 <Nav.Link onClick={logout}>Logout</Nav.Link>
@@ -47,10 +49,10 @@ function NavMenu(props) {
                     {!user &&
                         <Nav>
                             <LinkContainer to="login">
-                                <Nav.Link>Login</Nav.Link>
+                                <Nav.Link bsPrefix="navLink">Login</Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="signup">
-                                <Nav.Link>Sign Up</Nav.Link>
+                                <Nav.Link bsPrefix="navLink">Sign Up</Nav.Link>
                             </LinkContainer>
                         </Nav>
 
