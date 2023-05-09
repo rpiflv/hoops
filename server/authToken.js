639b1517085a49eb3ff034f02012d43e5a1090e4
rpiflv <rpiflv@gmail.com>
2023-05-09 18:32:56 +0900

    checked server code

--- server/authToken.js
+++ server/authToken.js
@@ -1,21 +1,17 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-
-console.log(process.env.ACCESS_TOKEN_SECRET)
 const authToken = async (req, res, next) => {
     const token = req.header("x-auth-token");
-
     if (!token) {
-        res.send("missing token")
+        res.send("missing token");
     }
-
     try {
-        const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         req.user = user.email;
         next();
     } catch (err) {
-        res.status(403).send("Token not correct")
+        res.status(403).send("Token not correct");
     }
 }
 
