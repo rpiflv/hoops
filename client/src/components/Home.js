d0d18e4c10cd1ae48719800db8f1e1f38997d571
rpiflv <rpiflv@gmail.com>
2023-05-09 22:45:05 +0900

    fix elòint in home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import {Link} from 'react-router-dom';
-import ListGroup from 'react-bootstrap/ListGroup';
 import axios from "axios";
-import { Card, Col, Container, Row, Stack } from "react-bootstrap";
-import { MDBSwitch } from 'mdb-react-ui-kit';
+import { Card, Col, Container } from "react-bootstrap";
 import '../App.css';
 import LiveScores from "./LiveScores";
 
@@ -14,31 +11,26 @@ function Home() {
     const [lastNews, setLastNews] = useState([]);
     const [showScore, setShowScore] = useState(true);
     const [firstComponentHeight, setFirstComponentHeight] = useState(0);
-
-    const getNews = async () => {
-        try {
-            const fetchedData = await axios.get(BASE_URL + '/api/')
-            setLastNews(fetchedData.data[0].articles)
-            
-        } catch (error) {
-            console.log(error)
-        }
-    }  
-
+    
     useEffect(() => {
+        const getNews = async () => {
+            try {
+                const fetchedData = await axios.get(BASE_URL + '/api/')
+                setLastNews(fetchedData.data[0].articles)
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }  
         getNews()
     }, [])
 
-    const toggleScore = () => {
-        setShowScore(!showScore)
-    }
-
     useEffect(() => {
+        const toggleScore = () => {
+            setShowScore(!showScore)
+        }
         toggleScore()
-    }, [])
-
-    
-
+    }, [showScore])
 
     const handleFirstComponentHeight = (height) => {
         setFirstComponentHeight(height);
@@ -49,15 +41,14 @@ function Home() {
         <br/>
         {/* {console.log(lastNews)} */}
         <Container>
-          <LiveScores onHeightChange={handleFirstComponentHeight}/>
+            <LiveScores onHeightChange={handleFirstComponentHeight}/>
             <br/>
-                <h1 style={{marginTop:firstComponentHeight + 5}}>Latest News</h1>
-                <br />
+            <h1 style={{marginTop:firstComponentHeight + 5}}>Latest News</h1>
+            <br/>
             <Container className="d-flex align-items-center justify-content-center news-container" >
                 <Col md='8' >          
                     {lastNews?.map((news, index) => (
-                        
-                        <a href={news.links.web.href} style={{textDecoration:"none", color:"darkgray"}} target="_blank">
+                        <a href={news.links.web.href} style={{textDecoration:"none", color:"darkgray"}} target="_blank" rel="noreferrer">
                         <Card key={index} className="box-news mb-3"    >
                             <Card.Title style={{color: "black"}}> {news.headline}</Card.Title>
                             <div className="row no-gutters">
