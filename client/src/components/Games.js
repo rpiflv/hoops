bfedb4ad542eaf1ae7d3e929044744593215c580
rpiflv <rpiflv@gmail.com>
2023-05-09 19:03:56 +0900

    games cleaned

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card, Container, Col, Button , Row} from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Button.scss";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || '';
@@ -10,26 +10,25 @@ function Games () {
     const d = new Date();
     
     const [games, setGames] = useState([]);
-    
     const [day, setDay] = useState(d.getDate());
     const [month, setMonth]  = useState(d.getMonth() + 1);
     const [year, setYear] = useState(d.getFullYear());
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
 
     const getMatches = async () => {
-        setLoading(true)
+        setLoading(true);
         try {
-            const fetchedData = await axios.get(BASE_URL + `/api/games/${year}/${month}/${day}`)
-            setGames(fetchedData.data)
-            setLoading(false)
+            const fetchedData = await axios.get(BASE_URL + `/api/games/${year}/${month}/${day}`);
+            setGames(fetchedData.data);
+            setLoading(false);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
     
     useEffect(() => {
         getMatches();
-    }, [day, month, year])
+    }, [day, month, year]);
 
     const handleDateChange = (amount) => {
         const newDate = new Date (year, month - 1, day + amount);
@@ -52,7 +51,6 @@ function Games () {
             <Col>
                 <Button onClick={() => {handleDateChange(1)}} disabled={loading} className="Button day next-day shadow-sm"> Next day </Button>
                 <Button onClick={() => {handleDateChange(7)}} disabled={loading} className="Button week next-week shadow-sm">Next week </Button>
-
             </Col>
         </Row>
         </div>
@@ -60,96 +58,82 @@ function Games () {
         <Row >
         {
             games?.games?.map((game) => 
-             
-
-            
             <Col key={game.id}>
-                {console.log(game)}
-                    { 
-                        game.status !== "unnecessary" ? 
-                    
-                   <Card className="games-box shadow-sm">
-                   <Link style={{textDecoration: "none"}} to={game.id} className="link-light">
-                   <Card.Title className="games-box-title text-truncate">
-                   {game.title}
-                   </Card.Title>
-                <Card.Body style={{fontStyle: "oblique"}}>
-                    <Row className=" d-flex justify-content-center">
-                        <Col className="col-md-5 justify-content-md-center">
-                            <Row className="justify-content-md-center">
-                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.away.alias.toLowerCase()}.png`} 
-                                style={{width:"20rem", marginRight:"1px", marginRight:"1px"}}/>
-                            </Row>
-                            <Row className="justify-content-md-center">
-                                {game.away.alias} 
-                            </Row>
-                        </Col>
-                        <Col className="col-md-2 d-flex align-items-center justify-content-md-center">VS</Col>
-                        <Col className="col-md-5">
-                        <Row className="justify-content-md-center">
-
-                   <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.home.alias.toLowerCase()}.png`} 
-                   style={{width:"20rem", marginLeft:"1px", marginRight:"1px"}}/> 
-                   </Row>
-                   <Row className="justify-content-md-center">
-
-                   {game.home.alias} 
-                   </Row>
-                   </Col>
-                   </Row>
-                   </Card.Body>
-                   </Link>
-                   </Card> 
-                   :
-                   <span>
-                   <Card className="games-box unnecessary shadow-sm">
-                   <Link style={{textDecoration: "none"}} to={game.id} className="link-light">
-                   <Card.Title className="games-box-title text-truncate">
-                   {game.title}
-                   <Card.Subtitle>
-
-                        {game.status}
-                   </Card.Subtitle>
-                   </Card.Title>
-                   <Card.Body>
-                    <Row className=" d-flex justify-content-center">
-                        <Col className="col-md-5 justify-content-md-center">
-                            <Row className="justify-content-md-center">
-                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.away.alias.toLowerCase()}.png`} 
-                                style={{width:"5rem", marginRight:"1px", marginRight:"1px"}}/>
-                            </Row>
-                            <Row className="justify-content-md-center">
-                                {game.away.alias} 
-                            </Row>
-                        </Col>
-                        <Col className="col-md-2 d-flex align-items-center justify-content-md-center">VS</Col>
-                        <Col className="col-md-5">
-                            <Row className="justify-content-md-center">
-                             <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.home.alias.toLowerCase()}.png`} 
-                                style={{width:"5rem", marginLeft:"1px", marginRight:"1px"}}/> 
-                            </Row>
-                   <Row className="justify-content-md-center">
-
-                   {game.home.alias} 
-                   </Row>
-                   </Col>
-                   </Row>
-                   </Card.Body>
-                   </Link>
-                   </Card> 
-                   </span>
-                     
+                { game.status !== "unnecessary" ? 
+                    <Card className="games-box shadow-sm">
+                        <Link style={{textDecoration: "none"}} to={game.id} className="link-light">
+                            <Card.Title className="games-box-title text-truncate">
+                            {game.title}
+                            </Card.Title>
+                            <Card.Body style={{fontStyle: "oblique"}}>
+                                <Row className=" d-flex justify-content-center">
+                                    <Col className="col-md-5 justify-content-md-center">
+                                        <Row className="justify-content-md-center">
+                                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.away.alias.toLowerCase()}.png`} 
+                                            style={{width:"20rem", marginRight:"1px", marginRight:"1px"}}/>
+                                        </Row>
+                                        <Row className="justify-content-md-center">
+                                            {game.away.alias} 
+                                        </Row>
+                                    </Col>
+                                    <Col className="col-md-2 d-flex align-items-center justify-content-md-center">
+                                        VS
+                                    </Col>
+                                    <Col className="col-md-5">
+                                        <Row className="justify-content-md-center">
+                                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.home.alias.toLowerCase()}.png`} 
+                                            style={{width:"20rem", marginLeft:"1px", marginRight:"1px"}}/> 
+                                        </Row>
+                                        <Row className="justify-content-md-center">
+                                            {game.home.alias} 
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Link>
+                    </Card> 
+                    :
+                    <Card className="games-box unnecessary shadow-sm">
+                        <Link style={{textDecoration: "none"}} to={game.id} className="link-light">
+                            <Card.Title className="games-box-title text-truncate">
+                                {game.title}
+                            </Card.Title>
+                            <Card.Subtitle>
+                                {game.status}
+                            </Card.Subtitle>
+                            <Card.Body>
+                                <Row className=" d-flex justify-content-center">
+                                    <Col className="col-md-5 justify-content-md-center">
+                                        <Row className="justify-content-md-center">
+                                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.away.alias.toLowerCase()}.png`} 
+                                            style={{width:"5rem", marginRight:"1px", marginRight:"1px"}}/>
+                                        </Row>
+                                        <Row className="justify-content-md-center">
+                                            {game.away.alias} 
+                                        </Row>
+                                    </Col>
+                                    <Col className="col-md-2 d-flex align-items-center justify-content-md-center">
+                                        VS
+                                        </Col>
+                                    <Col className="col-md-5">
+                                        <Row className="justify-content-md-center">
+                                            <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${game?.home.alias.toLowerCase()}.png`} 
+                                            style={{width:"5rem", marginLeft:"1px", marginRight:"1px"}}/> 
+                                        </Row>
+                                        <Row className="justify-content-md-center">
+                                            {game.home.alias} 
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Link>
+                    </Card> 
                 }
-                   </Col>
-
-                   )
-                
-                   
-                }
+            </Col>
+        )}
         </Row>
-    </Container>
+        </Container>
     </>
-        
     )
 }
 
