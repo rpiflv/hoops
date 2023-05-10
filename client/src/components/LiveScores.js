e61a6e1de8b4eff26528b360416a82f139c516bf
rpiflv <rpiflv@gmail.com>
2023-05-10 12:34:11 +0900

    livescores cleaned

--- client/src/components/LiveScores.js
+++ client/src/components/LiveScores.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
-import {Link} from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import axios from "axios";
-import { Card, Col, Container, Row, Stack } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { MDBSwitch } from 'mdb-react-ui-kit';
 import '../App.css';
 
@@ -10,7 +9,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || '';
 
 function LiveScores ({onHeightChange}) {
 
-    const ref = useRef()
+    const ref = useRef();
 
     const [todaysMatches, setTodaysMatches] = useState([]);
     const [showScore, setShowScore] = useState(false);
@@ -20,9 +19,9 @@ function LiveScores ({onHeightChange}) {
     const getMatches = async () => {
         try {
             const fetchedData = await axios.get(BASE_URL + '/api/')
-            setTodaysMatches(fetchedData.data[1].scoreboard.games)
+            setTodaysMatches(fetchedData.data[1].scoreboard.games);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
 
@@ -46,13 +45,13 @@ function LiveScores ({onHeightChange}) {
     useEffect(() => {
         if (ref.current) {
             const height = ref.current.clientHeight;
-            onHeightChange(height)
+            onHeightChange(height);
         }
-    }, [onHeightChange])
+    }, [onHeightChange]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-          getMatches()
+          getMatches();
         }, 5000);
     
         return () => clearInterval(interval);
@@ -60,11 +59,11 @@ function LiveScores ({onHeightChange}) {
 
 
     useEffect(() => {
-        getMatches()
+        getMatches();
     }, [])
 
     const toggleScore = () => {
-        setShowScore(!showScore)
+        setShowScore(!showScore);
     }
 
     return (
@@ -76,26 +75,23 @@ function LiveScores ({onHeightChange}) {
                     <Row className="justify-content-center align-items-center">
                         {todaysMatches?.length > 0 ? todaysMatches?.map((match, index) =>
                             <Card style={{ width: 'auto', padding: "1rem", margin: "3px" }} className="text-center live-box shadow" key={index} >
-                                {/* <Card.Body> */}
-                                        <Card.Title>
-                                            <Row>
-                                            <Col>
+                                <Card.Title>
+                                    <Row>
+                                        <Col>
                                             <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${match.awayTeam.teamTricode.toLowerCase()}.png`} style={{width:"2rem", marginLeft:"1px", marginRight:"1px"}}></img>
                                             <strong>{match.awayTeam.teamTricode}</strong >
                                          
                                             <span style={{color: "gray"}}> vs </span>
                                             <strong>{match.homeTeam.teamTricode}</strong >
                                             <img src={`https://a.espncdn.com/i/teamlogos/nba/500-dark/${match.homeTeam.teamTricode.toLowerCase()}.png`} style={{width:"2rem"}}></img>
-                                            </Col>
-                                            </Row>
-                                            
-                                        </Card.Title>
-                                        <ListGroup.Item className="live-score">{match.gameStatus === 2 &&
-                                                <span className={"live"}>LIVE</span>
-                                            }    score: {showScore ? <div>{match.awayTeam.score} : {match.homeTeam.score}</div>
-                                            : <div>-- : --</div>}
-                                        </ListGroup.Item>
-                                {/* </Card.Body> */}
+                                        </Col>
+                                    </Row>  
+                                </Card.Title>
+                                    <ListGroup.Item className="live-score">{match.gameStatus === 2 &&
+                                            <span className={"live"}>LIVE</span>
+                                        }    score: {showScore ? <div>{match.awayTeam.score} : {match.homeTeam.score}</div>
+                                        : <div>-- : --</div>}
+                                    </ListGroup.Item>
                             </Card>
                                 ) 
                                 :
@@ -104,7 +100,7 @@ function LiveScores ({onHeightChange}) {
                     </Row>
                     </div>
                     <div className="col-md-2 d-flex align-items-center justify-content-center">
-                    <label htmlFor="showscore" className="ms-auto">show scores</label>
+                        <label htmlFor="showscore" className="ms-auto">show scores</label>
                     <MDBSwitch id="showscore" onClick={toggleScore}/>    
                     </div>
                 </Row>
