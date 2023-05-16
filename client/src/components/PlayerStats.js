91a47f0f07864a7eecbbecfb006b964d3de4e838
rpiflv <rpiflv@gmail.com>
2023-05-16 11:11:52 +0900

    added more player stats

--- client/src/components/PlayerStats.js
+++ client/src/components/PlayerStats.js
@@ -77,7 +77,6 @@ function PlayerStats(props) {
                         {team.average.points}
                         </td>
                         <td>
-                        
                         {team.average.points_in_paint}
                         </td>
                         <td>
@@ -114,6 +113,134 @@ function PlayerStats(props) {
                         </td>
                         <td>
                         {team.average.off_rebounds}
+                        </td>
+                    </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th style={{width:"25%"}}>
+                        2Pts FG%
+                    </th>
+                    <th style={{width:"25%"}}>
+                        2Pts Attempts
+                    </th>
+                    <th style={{width:"25%"}}>
+                        3Pts FG%
+                    </th>
+                    <th style={{width:"25%"}}>
+                        3Pts Attempts
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        {team.total.two_points_pct}
+                        </td>
+                        <td>
+                        {team.average.two_points_att}
+                        </td>
+                        <td>
+                        {team.total.three_points_pct}
+                        </td>
+                        <td>
+                        {team.average.three_points_att}
+                        </td>
+                    </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th style={{width:"25%"}}>
+                        FT%
+                    </th>
+                    <th style={{width:"25%"}}>
+                        Avg. FT Attempts
+                    </th>
+                    <th style={{width:"25%"}}>
+                        Eff. FG%
+                    </th>
+                    <th style={{width:"25%"}}>
+                       True Shooting%
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        {team.total.free_throws_pct}
+                        </td>
+                        <td>
+                        {team.average.free_throws_att}
+                        </td>
+                        <td>
+                        {team.total.effective_fg_pct}
+                        </td>
+                        <td>
+                        {team.total.true_shooting_pct}
+                        </td>
+                    </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th style={{width:"25%"}}>
+                        Assist
+                    </th>
+                    <th style={{width:"25%"}}>
+                        Avg Assist
+                    </th>
+                    <th style={{width:"25%"}}>
+                        TurnOver
+                    </th>
+                    <th style={{width:"25%"}}>
+                       Avg. TO
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        {team.total.assists}
+                        </td>
+                        <td>
+                        {team.average.assists}
+                        </td>
+                        <td>
+                        {team.total.turnovers}
+                        </td>
+                        <td>
+                        {team.average.turnovers}
+                        </td>
+                    </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th style={{width:"25%"}}>
+                        Steals
+                    </th>
+                    <th style={{width:"25%"}}>
+                        Avg Steals
+                    </th>
+                    <th style={{width:"25%"}}>
+                        Blocks
+                    </th>
+                    <th style={{width:"25%"}}>
+                       Avg. Blocks
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        {team.total.steals}
+                        </td>
+                        <td>
+                        {team.average.steals}
+                        </td>
+                        <td>
+                        {team.total.blocks}
+                        </td>
+                        <td>
+                        {team.average.blocks}
                         </td>
                     </tr>
                 </tbody>
