fe66a75774442c2d486c7809f2970d314e6a197f
rpiflv <rpiflv@gmail.com>
2023-05-30 22:46:47 +0900

    add migaration for full name in fav player

---
+++ server/db/migrations/20230530130608_add_fullneme_to_favplayers.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('fav_players', function (table) {
+        table.string('full_name');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('fav_players', function (table) {
+        table.dropColumn('full_name');
+      });
+};
