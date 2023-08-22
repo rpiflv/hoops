8a994cb900949c21007993c112a9abbbb3b38926
rpiflv <rpiflv@gmail.com>
2023-08-22 22:49:24 +0900

    test navbar login

--- client/src/components/__tests__/NavMenu.test.js
+++ client/src/components/__tests__/NavMenu.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavMenu from "../NavMenu";
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from "react-router-dom";
 
 const MockNavMenu = () => {
     return (
         <BrowserRouter>
-            <NavMenu />
+            <NavMenu user={
+            {
+            accessToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZsYXZpb0BlbWFpbC5jb20iLCJpYXQiOjE2OTI3MDYxOTQsImV4cCI6MTY5MjcwOTc5NH0.-gYWUsuUT3zH3RPmecnZw_IFEV6vky9TTVJHb2DIHQ0",
+            refreshToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZsYXZpb0BlbWFpbC5jb20iLCJpYXQiOjE2OTI3MDYxOTQsImV4cCI6MTcwMDQ4MjE5NH0.NG7gcrXmG17Z1IxY6WuET3ZFSmY5d2s9vpbdNkhyS-8",
+            user_id:1
+            }
+        }/>
         </BrowserRouter>
     )
 }
@@ -23,5 +29,12 @@ describe("Navbar", () => {
         render(<MockNavMenu/>);
         const linkElementNBATeams = await screen.findByText("NBA Teams");
         expect(linkElementNBATeams).toBeInTheDocument();
+    });
+
+    it("It shows My Players link if signed in", async () => {
+        render(<MockNavMenu />);
+        const linkElementMyPlayers = await screen.findByText("My Players");
+        waitFor(() => expect(linkElementMyPlayers).toBeInTheDocument()); 
+
     })
 })
